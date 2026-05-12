80aeabe3cb858ca8f41974a564ba8b7e9f39b122
Pablo Gramajo <pdp1985gramajo@gmail.com>
2026-05-12 15:29:28 -0300

    chore: configure eslint and convert to arrow functions

--- eslint.config.js
+++ eslint.config.js
@@ -17,5 +17,9 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: {
+      'prefer-arrow-callback': 'error',
+      'arrow-spacing': ['error', { before: true, after: true }],
+    },
   },
 ])
