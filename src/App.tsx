50109356080d709fba61880c584d85a000f941c4
Pablo Gramajo <pdp1985gramajo@gmail.com>
2026-05-12 16:07:39 -0300

    feat: add Tailwind, Vitest, code splitting, env config and husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
 import './App.css'
+
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
+
+const Loading = () => <div>Loading...</div>
 
 const App = () => (
   <div>
     <nav>
       <Link to="/">Home</Link> | <Link to="/about">About</Link>
     </nav>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Suspense>
   </div>
 )
 
